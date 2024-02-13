be7791f3beb0afb7c99ce336d452463cb433c7f9
Matthew Kirk Andrews <93593941+shoowadoo@users.noreply.github.com>
2024-02-13 17:31:37 +0100

    upload gwichin

---
+++ gwichin-dictionary/clean.py
@@ -0,0 +1,25 @@
+import re
+
+def remove_patterns_from_file(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
+
+    # Remove question marks
+    text = text.replace('?', '')
+
+    # Remove "page + num" pattern
+    text = re.sub(r'page \d+', '', text)
+    
+    text = text.replace('Check!', '')
+    
+    text = text.replace('=', '')
+
+    # Save the modified text back to the file
+    with open("dictionary.txt", 'w') as file:
+        file.write(text)
+
+# Specify the path to your text file
+file_path = 'KU960M1991_March1994.txt'
+
+# Call the function to remove the patterns from the file
+remove_patterns_from_file(file_path)
---
+++ gwichin-dictionary/dictionary.py
@@ -0,0 +1,66 @@
+import json
+
+# Read the text file with UTF-8 encoding
+with open('dictionary.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+
+# Split the text into lines
+lines = text.split('\n')
+
+# Define an empty dictionary to store the entries
+englishToGwichin = {}
+gwichinToEnglish = {}
+
+# Iterate over each line of the text
+for line in lines:
+    # Trim any leading or trailing whitespace
+    line = line.strip()
+    
+    # Skip empty lines and lines starting with " " or upper case
+    # if line == '' or line.startswith(' ') or line[0].isupper():
+    if line == '' or line.startswith(' ') or 'accidental' in line or 'rare' in line or 'hypothetical' in line:
+        continue
+    
+    # Split the line into Gwichin and English parts using " " as the separator
+    parts = line.split(' ')
+        
+    if len(parts) > 1 and parts[1].strip() != "":
+        # Extract the Gwichin word and English translation
+        gwichin_word = parts[0].strip()
+        english_word = parts[1].strip()
+    
+    #initial_letter = gwichin_word[0].upper()
+        
+    ##if initial_letter not in englishToGwichin:
+        ##dictionary[initial_letter] = {}
+
+    # Add the entry to the dictionary under the initial letter
+
+    #dictionary[initial_letter][gwichin_word] = english_translation
+        if gwichin_word in gwichinToEnglish:
+        #print('Duplicate entry found:', gwichin_word)
+            gwichinToEnglish[gwichin_word].append(english_word)
+        else:
+            gwichinToEnglish[gwichin_word] = [english_word]
+        
+        if english_word in englishToGwichin: 
+            englishToGwichin[english_word].append(gwichin_word)
+        else:
+            englishToGwichin[english_word] = [gwichin_word]
+        
+#print(dict(sorted(englishToGwichin.items())))
+
+json_data = json.dumps(dict(sorted(gwichinToEnglish.items())), indent=2, ensure_ascii=False)
+
+# Save the JSON to a file
+with open('gwichinToEnglish.json', 'w', encoding='utf-8') as file:
+    file.write(json_data)
+    
+json_data = json.dumps(dict(sorted(englishToGwichin.items())), indent=2, ensure_ascii=False)
+
+# Save the JSON to a file
+with open('englishToGwichin.json', 'w', encoding='utf-8') as file:
+    file.write(json_data)
+
+#print(len(englishToGwichin))
+#print(len(gwichinToEnglish))
