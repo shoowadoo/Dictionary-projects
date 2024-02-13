73ca9b2e72ef33d25b2c848b7415c24ed80b7c56
Matthew Kirk Andrews <93593941+shoowadoo@users.noreply.github.com>
2024-02-13 17:38:52 +0100

    Delete esperanto-dictionary directory

--- esperanto-dictionary/practice.js
+++
@@ -1,5 +0,0 @@
-const word = "abort";
-
-const new_word = word.charAt(0).toUpperCase() + word.slice(1);
-
-console.log(new_word);
--- esperanto-dictionary/test.py
+++
@@ -1,6 +0,0 @@
-my_dict = {"A": {"Apple": "Apfel", "Ass": "Arsch"}, 
-           "B": {"Bed": "Bett", "Bird": "Vogel"}}
-
-my_dict["A"]["Ash"] = "Asche"
-
-print(my_dict)
