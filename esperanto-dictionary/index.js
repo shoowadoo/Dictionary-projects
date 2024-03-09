203ea2a0e6e8be67174c7f44bde2363f97d10d07
shoowadoo <93593941+shoowadoo@users.noreply.github.com>
2024-03-09 22:15:36 +0100

    same to es

--- esperanto-dictionary/index.js
+++ esperanto-dictionary/index.js
@@ -73,5 +73,6 @@ inputEl.addEventListener("keyup", (e) => {
     if (e.target.value && e.key === "Enter") {
         fetchAPI(e.target.value);
         e.target.value = "";
+        e.target.blur()
     }
 });
